e81a71be300c533cbd89cab28bc003c4b271f9e6
ro011110ot <ro011110ot@gmail.com>
2025-11-20 12:11:02 +0100

    Save current work before cargo fix

---
+++ lvgl/src/main.rs
@@ -0,0 +1,14 @@
+use esp_idf_hal::sys;
+use esp_idf_svc::log::EspLogger;
+use esp_idf_svc::sys::link_patches;
+use esp_idf_sys as _; // link patches
+fn main() {
+    // It is necessary to call this function once. Otherwise, some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Hello, world!");
+}
